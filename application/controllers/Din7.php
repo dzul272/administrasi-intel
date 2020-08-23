84500876ba8547a4bf8085284c9ca10ee32670f7
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-08-23 09:53:49 +0700

    DONE EDIT

--- application/controllers/Din7.php
+++ application/controllers/Din7.php
@@ -10,10 +10,34 @@ class Din7 extends Kejari_Controller
         $this->load->model("Kelurahan_model", "kelurahan");
     }
 
+    public function getKelurahan($id_kec = NULL)
+    {
+        $kelurahan = $this->kelurahan
+            ->where("id_kec", $id_kec)
+            ->order_by("nama", "ASC")
+            ->get_all();
+
+        if ($kelurahan) {
+            echo json_encode([
+                "status"    => 200,
+                "message"   => "Data ditemukan",
+                "data"      => $kelurahan
+            ]);
+        } else {
+            echo json_encode([
+                "status"    => 400,
+                "message"   => "Data tidak ditemukan",
+                "data"      => []
+            ]);
+        }
+    }
+
     public function index()
     {
         redirect(base_url("din7/penerangan-hukum"));
     }
+
+    //! PENERANGAN HUKUM ================================================================
 
     public function penerangan_hukum()
     {
@@ -54,14 +78,26 @@ class Din7 extends Kejari_Controller
 
     public function getDataPeneranganHukum($tahun = NULL, $triwulan = NULL)
     {
-        $kondisi["YEAR(waktu)"] = $tahun;
+        $id = $this->input->get("id");
         $kondisi["jenis"]       = 1;
-        if ($triwulan !== "semua") {
-            $kondisi["triwulan"] = $triwulan;
+
+        if ($tahun) {
+            $kondisi["YEAR(waktu)"] = $tahun;
         }
 
+        if ($triwulan) {
+            if ($triwulan !== "semua") {
+                $kondisi["triwulan"] = $triwulan;
+            }
+        }
+
+        if ($id) {
+            $kondisi["id"]  = $id;
+        }
+
+
         $data = $this->din7
-            ->where($kondisi)            
+            ->where($kondisi)
             ->with_provinsi()
             ->with_kabupaten()
             ->with_kecamatan()
@@ -72,8 +108,8 @@ class Din7 extends Kejari_Controller
 
         if ($data) {
             for ($i = 0; $i < sizeof($data); $i++) {
-                $data[$i]["waktu_indo"] = longdate_indo($data[$i]["waktu"], TRUE);
-                $data[$i]["waktu_pelaksanaan_indo"] = longdate_indo($data[$i]["waktu_pelaksanaan"], TRUE);
+                $data[$i]["waktu_indo"] = $data[$i]["waktu"] != "000-00-00" ?  longdate_indo($data[$i]["waktu"], TRUE) : "";
+                $data[$i]["waktu_pelaksanaan_indo"] = $data[$i]["waktu_pelaksanaan"] != "0000-00-00" ? longdate_indo($data[$i]["waktu_pelaksanaan"], TRUE) : "";
             }
             echo json_encode([
                 "status"    => 200,
@@ -129,24 +165,39 @@ class Din7 extends Kejari_Controller
         }
     }
 
-    public function getKelurahan($id_kec = NULL)
+    public function updatePeneranganHukum()
     {
-        $kelurahan = $this->kelurahan
-            ->where("id_kec", $id_kec)
-            ->order_by("nama", "ASC")
-            ->get_all();
+        $input  = (object) $this->input->post();
 
-        if ($kelurahan) {
+        $dataUpdate = [
+            "peserta"               => $input->peserta,
+            "materi_tema"           => $input->materi_tema,
+            "jml_peserta"           => $input->jml_peserta,
+            "waktu"                 => $input->waktu,
+            "tempat_prov"           => 33,
+            "tempat_kab"            => 3302,
+            "tempat_kec"            => $input->tempat_kec,
+            "tempat_kel"            => $input->tempat_kel,
+            "tempat_detail"         => $input->tempat_detail,
+            "media"                 => $input->media,
+            "materi"                => $input->materi,
+            "waktu_pelaksanaan"     => $input->waktu_pelaksanaan,
+            "keterangan"            => $input->keterangan,
+
+            "jenis"                 => 1,
+            "triwulan"              => triwulan($input->waktu),
+        ];
+
+        $update = $this->din7->update($dataUpdate, $input->id_data);
+        if ($update) {
             echo json_encode([
-                "status"    => 200,
-                "message"   => "Data ditemukan",
-                "data"      => $kelurahan
+                'response_code'     => 200,
+                'response_message'  => 'Data Berhasil diupdate',
             ]);
         } else {
             echo json_encode([
-                "status"    => 400,
-                "message"   => "Data tidak ditemukan",
-                "data"      => []
+                'response_code'     => 200,
+                'response_message'  => 'Data Gagal diupdate',
             ]);
         }
     }
--- application/views/din7/pelaksanaan_hukum.php
+++ application/views/din7/pelaksanaan_hukum.php
@@ -45,7 +45,7 @@
         </div>
 
         <!-- MODAL TAMBAH DATA -->
-        <div class="modal fade myModal" id="tambahData" role="dialog" aria-labelledby="tambahPerdes">
+        <div class="modal fade myModal" id="tambahData" role="dialog">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header bg-success">
@@ -140,31 +140,83 @@
         <!-- END MODAL -->
 
         <!-- MODAL UBAH-->
-        <div class="modal fade myModal" id="modal_ubah" tabindex="-1" role="dialog" aria-labelledby="modal_ubah">
-            <div class="modal-dialog" role="document">
+        <div class="modal fade myModal" id="modal_ubah" role="dialog">
+            <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header bg-primary">
-                        <h4 class="modal-title text-white" id="exampleModalLabel1">Ubah Data</h4>
+                        <h4 class="modal-title text-white" id="exampleModalLabel1">Edit Data</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     </div>
-                    <form id="form-edit" enctype='multipart/form-data'>
+                    <form id="form-edit" method="POST" action="<?= base_url("din7/updatePeneranganHukum") ?>" enctype='multipart/form-data'>
                         <div class="modal-body">
+                            <label for="recipient-name" class="control-label text-info">Pelaksanaan Penerangan Hukum Secara Langsung</label>
                             <div class="form-group">
-                                <div class="box-foto-pamong">
-                                    <div class="bg-foto-profil-pamong">
-                                        <div id="imgPreview_edit" class="foto-profil-pamong" style="background-image: url('<?= asset("kejari/img/add.png") ?>')">
-                                        </div>
+                                <label for="recipient-name" class="control-label">Sasaran Peserta Penyuluhan Hukum <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" name="peserta" id="peserta_edit" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label">Materi / Tema Kegiatan <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" name="materi_tema" id="materi_tema_edit" required>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Jumlah Peserta (Orang) <span class="text-danger">*</span></label>
+                                        <input type="number" class="form-control" name="jml_peserta" id="jml_peserta_edit" min="0" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Waktu Pelaksanaan Kegiatan <span class="text-danger">*</span></label>
+                                        <input type="date" class="form-control" name="waktu" id="waktu_edit" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Kecamatan Kegiatan <span class="text-danger">*</span></label>
+                                        <select class="select2 form-control custom-select" style="width: 100%;" name="tempat_kec" id="tempat_kec_edit" required>
+                                            <option value="">Pilih Kecamatan</option>
+                                            <?php if ($kecamatan) : ?>
+                                                <?php foreach ($kecamatan as $kec) : ?>
+                                                    <option value="<?= $kec["id_kec"] ?>"><?= $kec["nama"] ?></option>
+                                                <?php endforeach ?>
+                                            <?php endif ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Desa / Kelurahan Kegiatan <span class="text-danger">*</span></label>
+                                        <select class="select2 form-control custom-select" style="width: 100%;" name="tempat_kel" id="tempat_kel_edit" disabled required>
+                                            <option value="">Pilih kecamatan terlebih dahulu</option>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="recipient-name" class="control-label">Pilih Gambar <span class="text-danger">*</span></label>
-                                <input accept="image/*" type="file" class="form-control-file" name="foto_simbol" id="imageUpload_edit">
-                                <small id="name1" class="text-info">Maximum Size : 5 Mb</small>
+                                <label for="recipient-name" class="control-label">Detail Tempat Kegiatan</label>
+                                <input type="text" class="form-control" name="tempat_detail" id="tempat_detail_edit">
                             </div>
+                            <label for="recipient-name" class="control-label text-info">Pelaksanaan Penerangan Hukum Secara Tidak Langsung</label>
                             <div class="form-group">
-                                <label for="recipient-name" class="control-label">Sektor <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" name="sektor" id="sektor_edit" required>
+                                <label for="recipient-name" class="control-label">Media Yang Digunakan</label>
+                                <input type="text" class="form-control" name="media" id="media_edit">
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Materi</label>
+                                        <input type="text" class="form-control" name="materi" id="materi_edit">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="recipient-name" class="control-label">Waktu Pelaksanaan Kegiatan</label>
+                                        <input type="date" class="form-control" name="waktu_pelaksanaan" id="waktu_pelaksanaan_edit">
+                                    </div>
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label for="recipient-name" class="control-label">Keterangan</span></label>
@@ -174,8 +226,7 @@
                         <div class="modal-footer">
                             <input type="hidden" name="id_data" id="id_data">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-primary ultra-disabled" id="add-btn">Simpan</button>
-
+                            <button type="submit" class="btn btn-success ultra-disabled" id="add-btn_edit">Simpan</button>
                         </div>
                     </form>
                 </div>
@@ -240,6 +291,32 @@
                         var nama = response.data[i]['nama'];
                         console.log(nama);
                         $('#tempat_kel').append('<option value="' + id + '">' + nama + '</option>')
+                    }
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    alert(xhr.responseText);
+                }
+            });
+        });
+
+        $("#tempat_kec_edit").change(function(e) {
+            $("#tempat_kel_edit").attr("disabled", true);
+            $("#tempat_kel_edit").empty();
+            $("#tempat_kel_edit").append("<option value=''>Loading...</option>");
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "<?= base_url("din7/getKelurahan/") ?>" + this.value,
+                success: function(response) {
+                    $("#tempat_kel_edit").attr("disabled", false);
+                    $("#tempat_kel_edit").empty();
+                    $("#tempat_kel_edit").append("<option value=''>Pilih Desa / Kelurahan</option>");
+                    var len = response.data.length;
+                    for (var i = 0; i < len; i++) {
+                        var id = response.data[i]['id_kel'];
+                        var nama = response.data[i]['nama'];
+                        // console.log(nama);
+                        $('#tempat_kel_edit').append('<option value="' + id + '">' + nama + '</option>')
                     }
                 },
                 error: function(xhr, ajaxOptions, thrownError) {
@@ -375,4 +452,124 @@
             }
         });
     }));
+
+    //TODO : FORM EDIT
+    $("#form-edit").on('submit', (function(event) {
+        event.preventDefault();
+        $('#add-btn_edit').html("Sedang Menyimpan...");
+        $('#add-btn_edit').attr("disabled", true);
+        $('#tombol-tambah').attr("disabled", true);
+
+        $.ajax({
+            url: "<?= base_url('din7/updatePeneranganHukum') ?>",
+            type: "POST",
+            data: new FormData(this),
+            dataType: "JSON",
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(e) { // Ketika proses pengiriman berhasil                
+                if (e.response_code == 200) {
+                    table_data.ajax.reload(null, true);
+                    $('.myModal').modal('hide');
+                    $("#form-edit")[0].reset();                    
+                    Swal.fire(
+                        'Berhasil',
+                        e.response_message,
+                        'success'
+                    ).then((result) => {
+                        // $('#table_body').empty();
+                        // $('#table_body').html(e.output);
+                        $('#add-btn').html("Simpan");
+                        $('#add-btn').attr("disabled", false);
+                        $('#tombol-tambah').html("+ Tambah Data");
+                        $('#tombol-tambah').attr("disabled", false);
+                    })
+
+                } else {
+                    Swal.close();
+                    Swal.fire("Oops", e.response_message, "error");
+                    $('#add-btn').html("Simpan");
+                    $('#add-btn').attr("disabled", false);
+                    $('#tombol-tambah').html("+ Tambah Data");
+                    $('#tombol-tambah').attr("disabled", false);
+                }
+            },
+            error: function(xhr, ajaxOptions, thrownError) { // Ketika ada error
+                Swal.fire("Oops", xhr.responseText, "error");
+                $('#add-btn').html("Simpan");
+                $('#add-btn').attr("disabled", false);
+                $('#tombol-tambah').html("+ Tambah Data");
+                $('#tombol-tambah').attr("disabled", false);
+            }
+        });
+    }));
+
+    function edit(id) {
+        Swal.fire({
+            title: 'Mohon Tunggu Beberapa Saat',
+            text: 'Sedang Mengambil data...',
+            onBeforeOpen: () => {
+                Swal.showLoading();
+                $.ajax({
+                    type: 'GET',
+                    url: '<?= base_url('din7/getDataPeneranganHukum/') ?>',
+                    dataType: "json",
+                    data: {
+                        "id": id
+                    },
+                    success: function(e) {
+                        console.log(JSON.stringify(e));
+
+                        $('#peserta_edit').val(e.data[0].peserta);
+                        $('#materi_tema_edit').val(e.data[0].materi_tema);
+                        $('#jml_peserta_edit').val(e.data[0].jml_peserta);
+                        $('#waktu_edit').val(e.data[0].waktu);
+                        $('#waktu_edit').val(e.data[0].waktu);
+
+                        $('#tempat_kec_edit').val(e.data[0].tempat_kec);
+                        $('#tempat_kec_edit').trigger('change');
+
+                        $('#tempat_detail_edit').val(e.data[0].tempat_detail);
+                        $('#media_edit').val(e.data[0].media);
+                        $('#materi_edit').val(e.data[0].materi);
+                        $('#waktu_pelaksanaan_edit').val(e.data[0].waktu_pelaksanaan);
+                        $('#keterangan_edit').val(e.data[0].keterangan);
+
+                        $('#id_data').val(e.data[0].id);
+
+                        $.ajax({
+                            type: "GET",
+                            dataType: "json",
+                            url: "<?= base_url("din7/getKelurahan/") ?>" + e.data[0].tempat_kec,
+                            success: function(response) {
+                                Swal.close();
+                                $('#modal_ubah').modal('show');
+                                $("#tempat_kel_edit").attr("disabled", false);
+                                $("#tempat_kel_edit").empty();
+                                $("#tempat_kel_edit").append("<option value=''>Pilih Desa / Kelurahan</option>");
+                                var len = response.data.length;
+                                for (var i = 0; i < len; i++) {
+                                    var id = response.data[i]['id_kel'];
+                                    var nama = response.data[i]['nama'];
+                                    console.log(nama);
+                                    $('#tempat_kel_edit').append('<option value="' + id + '">' + nama + '</option>')
+                                }
+                                $('#tempat_kel_edit').val(e.data[0].tempat_kel);
+                                $('#tempat_kel_edit').trigger('change');
+                            },
+                            error: function(xhr, ajaxOptions, thrownError) {
+                                Swal.close();
+                                alert(xhr.responseText);
+                            }
+                        });
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) { // Ketika ada error
+                        Swal.close();
+                        Swal.fire("Oops", xhr.responseText, "error");
+                    }
+                });
+            }
+        });
+    }
 </script>
