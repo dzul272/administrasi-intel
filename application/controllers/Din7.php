087b39015a5e07ca6b574d36d0fdf3f173055c41
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-08-23 22:44:01 +0700

    DONE DIN 7

--- application/controllers/Din7.php
+++ application/controllers/Din7.php
@@ -289,7 +289,7 @@ class Din7 extends Kejari_Controller
 
         $mpdf = new \Mpdf\Mpdf();
         $mpdf->WriteHTML($this->load->view('din7/penerangan_hukum/export_sederhana', $result, TRUE));
-        $filename = "D.IN.7_PENERANGAN_HUKUM_" . date("d_m_Y_H_i_s") . ".pdf";
+        $filename = "D.IN.7_PENERANGAN_HUKUM_SEDERHANA_" . date("d_m_Y_H_i_s") . ".pdf";
         $mpdf->Output($filename, 'I');
     }
 
@@ -495,7 +495,213 @@ class Din7 extends Kejari_Controller
 
         $mpdf = new \Mpdf\Mpdf();
         $mpdf->WriteHTML($this->load->view('din7/penyuluhan_hukum/export_sederhana', $result, TRUE));
-        $filename = "D.IN.7_PENYULUHAN_HUKUM_" . date("d_m_Y_H_i_s") . ".pdf";
+        $filename = "D.IN.7_PENYULUHAN_HUKUM_SEDERHANA_" . date("d_m_Y_H_i_s") . ".pdf";
+        $mpdf->Output($filename, 'I');
+    }
+
+    //! JAKSA MASUK SEKOLAH ================================================================
+
+    public function jaksa_masuk_sekolah()
+    {
+        $tahun      = $this->input->get("tahun");
+        $triwulan   = $this->input->get("triwulan");
+
+        if ($tahun == NULL || $triwulan == NULL || !is_numeric($tahun)) {
+            $tahun      =  date("Y");
+            $triwulan   = triwulan(date("Y-m-d"));
+            redirect(base_url("din7/jaksa-masuk-sekolah?tahun=" . $tahun . "&triwulan=" . $triwulan));
+        } else {
+            if (is_numeric($triwulan)) {
+                if ($triwulan < 1 || $triwulan > 4) {
+                    $triwulan = triwulan(date("Y-m-d"));
+                    redirect(base_url("din7/jaksa-masuk-sekolah?tahun=" . $tahun . "&triwulan=" . $triwulan));
+                }
+            } else if ($triwulan !== "semua") {
+                $triwulan = triwulan(date("Y-m-d"));
+                redirect(base_url("din7/jaksa-masuk-sekolah?tahun=" . $tahun . "&triwulan=" . $triwulan));
+            }
+        }
+
+        $listTahun = $this->din7
+            ->fields()
+            ->select("DISTINCT(YEAR(created_at)) as tahun")
+            ->get_all();
+
+        // d($this->getDataPeneranganHukum($tahun, $triwulan));        
+
+        $data = [
+            "SidebarType"   => "mini-sidebar",
+            "kecamatan"     => $this->getKecamatan(),
+            "listTahun"     => $listTahun
+        ];
+
+        $this->loadViewKejari("din7/jaksa_masuk_sekolah/index", $data);
+    }
+
+    public function getDataJaksaMasukSekolah($tahun = NULL, $triwulan = NULL)
+    {
+        echo $this->getDataX($tahun, $triwulan, 3);
+    }
+
+    public function addJaksaMasukSekolah()
+    {
+        $input  = (object) $this->input->post();
+
+        $dataInput = [
+            "peserta"               => $input->peserta,
+            "materi_tema"           => $input->materi_tema,
+            "jml_peserta"           => $input->jml_peserta,
+            "waktu"                 => $input->waktu,
+            "tempat_prov"           => 33,
+            "tempat_kab"            => 3302,
+            "tempat_kec"            => $input->tempat_kec,
+            "tempat_kel"            => $input->tempat_kel,
+            "tempat_detail"         => $input->tempat_detail,
+            "media"                 => $input->media,
+            "materi"                => $input->materi,
+            "waktu_pelaksanaan"     => $input->waktu_pelaksanaan,
+            "keterangan"            => $input->keterangan,
+
+            "jenis"                 => 3,
+            "triwulan"              => triwulan($input->waktu),
+            "created_by"            => $this->userData->id,
+        ];
+
+        $insert = $this->din7->insert($dataInput);
+
+        if ($insert) {
+            //TODO : GET OUTPUT               
+            echo json_encode([
+                'response_code'     => 200,
+                'response_message'  => 'Data Berhasil Ditambahkan',
+            ]);
+        } else {
+            echo json_encode([
+                'response_code'     => 400,
+                'response_message'  => 'Data Gagal Ditambahkan'
+            ]);
+        }
+    }
+
+    public function updateJaksaMasukSekolah()
+    {
+        $input  = (object) $this->input->post();
+
+        $dataUpdate = [
+            "peserta"               => $input->peserta,
+            "materi_tema"           => $input->materi_tema,
+            "jml_peserta"           => $input->jml_peserta,
+            "waktu"                 => $input->waktu,
+            "tempat_prov"           => 33,
+            "tempat_kab"            => 3302,
+            "tempat_kec"            => $input->tempat_kec,
+            "tempat_kel"            => $input->tempat_kel,
+            "tempat_detail"         => $input->tempat_detail,
+            "media"                 => $input->media,
+            "materi"                => $input->materi,
+            "waktu_pelaksanaan"     => $input->waktu_pelaksanaan,
+            "keterangan"            => $input->keterangan,
+
+            "jenis"                 => 3,
+            "triwulan"              => triwulan($input->waktu),
+        ];
+
+        $update = $this->din7->update($dataUpdate, $input->id_data);
+        if ($update) {
+            echo json_encode([
+                'response_code'     => 200,
+                'response_message'  => 'Data Berhasil diupdate',
+            ]);
+        } else {
+            echo json_encode([
+                'response_code'     => 200,
+                'response_message'  => 'Data Gagal diupdate',
+            ]);
+        }
+    }
+
+    public function deleteJaksaMasukSekolah()
+    {
+        $id = $this->input->post("id_data");
+        $delete = $this->din7->delete(["id" => $id]);
+        if ($delete) {
+            echo json_encode([
+                'response_code'     => 200,
+                'response_message'  => 'Data Berhasil Dihapus',
+            ]);
+        } else {
+            echo json_encode([
+                'response_code'     => 400,
+                'response_message'  => 'Data Gagal Dihapus',
+            ]);
+        }
+    }
+
+    public function jaksa_masuk_sekolah_export_lengkap()
+    {
+        $tahun  = $this->input->get("tahun");
+
+        if ($tahun == NULL || !is_numeric($tahun)) {
+            redirect(base_url("din7/jaksa-masuk-sekolah-export-lengkap?tahun=" . date("Y")));
+        }
+
+        $data   = json_decode($this->getDataX($tahun, "semua", 3));
+        // d($data);
+        // $this->load->view('din7/jaksa_masuk_sekolah/export_lengkap', $data, FALSE);
+
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($this->load->view('din7/jaksa_masuk_sekolah/export_lengkap', $data, TRUE));
+        $filename = "D.IN.7_JAKSA_MASUK_SEKOLAH_LENGKAP_" . date("d_m_Y_H_i_s") . ".pdf";
+        // $mpdf->Output($filename, 'D');
+        $mpdf->Output($filename, 'I');
+    }
+
+    public function jaksa_masuk_sekolah_export_sederhana()
+    {
+        $tahun  = $this->input->get("tahun");
+
+        if ($tahun == NULL || !is_numeric($tahun)) {
+            redirect(base_url("din7/jaksa-masuk-sekolah-export-sederhana?tahun=" . date("Y")));
+        }
+        $triwulan1  = json_decode($this->getDataX($tahun, 1, 3))->data;
+        $triwulan2  = json_decode($this->getDataX($tahun, 2, 3))->data;
+        $triwulan3  = json_decode($this->getDataX($tahun, 3, 3))->data;
+        $triwulan4  = json_decode($this->getDataX($tahun, 4, 3))->data;
+        $max        = max(sizeof($triwulan1), sizeof($triwulan2), sizeof($triwulan3), sizeof($triwulan4));
+
+        $data["triwulan1"] = [];
+        $data["triwulan2"] = [];
+        $data["triwulan3"] = [];
+        $data["triwulan4"] = [];
+        for ($i = 0; $i < $max; $i++) {
+            if (isset($triwulan1[$i])) {
+                $data["triwulan1"][$i] = $triwulan1[$i];
+            }
+
+            if (isset($triwulan2[$i])) {
+                $data["triwulan2"][$i] = $triwulan2[$i];
+            }
+
+            if (isset($triwulan3[$i])) {
+                $data["triwulan3"][$i] = $triwulan3[$i];
+            }
+
+            if (isset($triwulan4[$i])) {
+                $data["triwulan4"][$i] = $triwulan4[$i];
+            }
+        }
+
+        $result = [
+            "max"   => $max,
+            "data"  => $data
+        ];
+
+        // d($result);
+        // $this->load->view('din7/jaksa_masuk_sekolah/export_sederhana', $result, FALSE);
+
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($this->load->view('din7/jaksa_masuk_sekolah/export_sederhana', $result, TRUE));
+        $filename = "D.IN.7_JAKSA_MASUK_SEKOLAH_SEDERHANA_" . date("d_m_Y_H_i_s") . ".pdf";
         $mpdf->Output($filename, 'I');
     }
 }
---
+++ application/views/din7/jaksa_masuk_sekolah/export_sederhana.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= asset('website/css/bootstrap.min.css') ?>">
+    <title>Export D.IN.7 - Jaksa Masuk Sekolah </title>
+</head>
+
+<body style="font-family: Times New Roman">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="text-left col-xs-6" style="margin-top: 0px; padding-left: 0px;">
+                <h5 style="font-size: 12px; font-family: Times New Roman;font-weight: bold; padding-bottom:0px;">
+                    <u>
+                        KEJAKSAAN NEGERI PURWOKERTO
+                    </u>
+                </h5>
+            </div>
+            <div class="text-right" style="margin-top: 0px">
+                <h5 style="font-size: 12px; font-family: Times New Roman;font-weight: bold; padding-bottom:0px;">
+                    D.IN.7
+                </h5>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xs-2">
+            <img src="<?= asset("kejari/img/logo.png") ?>" height="60px" style="margin-left: 20px; margin-top: 10px;" alt="Logo Kejari">
+        </div>
+        <div class="text-center" style="margin-top: 20px">
+            <h5 style="font-family: Times New Roman;font-weight: bold; padding-bottom:0px; font-size:large">
+                PELAKSANAAN KEGIATAN <br>
+                PROGRAM JAKSA MASUK SEKOLAH (JMS)
+            </h5>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="table" style="margin-top: 20px;">
+            <table class="table table-bordered" style="border: 1px solid black;">
+                <thead>
+                    <tr>
+                        <th style="padding: 5px;" class="align-middle text-center">No</th>
+                        <th style="padding: 5px;" class="align-middle text-center">Jenis</th>
+                        <th style="padding: 5px;" class="align-middle text-center">Triwulan I</th>
+                        <th style="padding: 5px;" class="align-middle text-center">Triwulan II</th>
+                        <th style="padding: 5px;" class="align-middle text-center">Triwulan III</th>
+                        <th style="padding: 5px;" class="align-middle text-center">Triwulan IV</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php for ($i = 0; $i < $max; $i++) : ?>
+                        <tr>
+                            <td class="text-center align-top" rowspan="4" style="padding: 4px;"><?= ($i + 1) ?></td>
+                            <td class="align-top" style="padding: 4px;">Tema Kegiatan</td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan1"][$i]) ? $data["triwulan1"][$i]->materi_tema : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan2"][$i]) ? $data["triwulan2"][$i]->materi_tema : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan3"][$i]) ? $data["triwulan3"][$i]->materi_tema : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan4"][$i]) ? $data["triwulan4"][$i]->materi_tema : ""  ?></td>
+                        </tr>
+                        <tr>
+                            <td class="align-top" style="padding: 4px;">Waktu</td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan1"][$i]) ? $data["triwulan1"][$i]->waktu_indo : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan2"][$i]) ? $data["triwulan2"][$i]->waktu_indo : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan3"][$i]) ? $data["triwulan3"][$i]->waktu_indo : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan4"][$i]) ? $data["triwulan4"][$i]->waktu_indo : ""  ?></td>
+                        </tr>
+                        <tr>
+                            <td class="align-top" style="padding: 4px;">Tempat</td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan1"][$i]) ? $data["triwulan1"][$i]->tempat_detail . " " . $data["triwulan1"][$i]->kelurahan->nama . ", Kec. " . $data["triwulan1"][$i]->kecamatan->nama   : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan2"][$i]) ? $data["triwulan2"][$i]->tempat_detail . " " . $data["triwulan2"][$i]->kelurahan->nama . ", Kec. " . $data["triwulan2"][$i]->kecamatan->nama   : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan3"][$i]) ? $data["triwulan3"][$i]->tempat_detail . " " . $data["triwulan3"][$i]->kelurahan->nama . ", Kec. " . $data["triwulan3"][$i]->kecamatan->nama   : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan4"][$i]) ? $data["triwulan4"][$i]->tempat_detail . " " . $data["triwulan4"][$i]->kelurahan->nama . ", Kec. " . $data["triwulan4"][$i]->kecamatan->nama   : ""  ?></td>
+                        </tr>
+                        <tr>
+                            <td class="align-top" style="padding: 4px;">Jumlah Peserta</td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan1"][$i]) ? $data["triwulan1"][$i]->jml_peserta . " Orang" : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan2"][$i]) ? $data["triwulan2"][$i]->jml_peserta . " Orang" : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan3"][$i]) ? $data["triwulan3"][$i]->jml_peserta . " Orang" : ""  ?></td>
+                            <td class="align-top" style="padding: 4px;"><?= isset($data["triwulan4"][$i]) ? $data["triwulan4"][$i]->jml_peserta . " Orang" : ""  ?></td>
+                        </tr>
+                    <?php endfor ?>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- <div class="row" style="height: 10px;">
+            <div class="col-xs-12">
+                <div class="text-left col-xs-6" style="margin-top: 0px; padding-left: 0px;;">
+                    <h5 style="font-size: 12px; font-family: Times New Roman;font-weight: bold; padding-bottom:0px;">
+
+                    </h5>
+                </div>
+                <div class="text-right" style="margin-top: 0px;">
+                    <div class="text-center" style="font-size: 12px; font-family: Times New Roman; padding-bottom:0px;">
+                        AN. KEPALA KEJAKSAAN NEGERI PURWOKERTO <br>
+                        KEPALA SEKSI INTELIJEN <br> <br> <br><br>
+                        <b><u>SURAYADI SEMBIRING, SH. MH.</u></b> <br>
+                        JAKSA MUDA NIP 19751110 200012 1 001
+                    </div>
+                </div>
+            </div>
+        </div> -->
+    </div>
+
+
+</body>
+
+</html>
